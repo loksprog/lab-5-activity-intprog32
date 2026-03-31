925801227b0e2066483f1e4836eacf7f125a95a1
Luke Harvey Umpad <lukeumpad9597@gmail.com>
2026-03-31 17:20:36 +0800

    feat: implemented  Database Helper

--- typescript-crud-api/src/_helpers/db.ts
+++ typescript-crud-api/src/_helpers/db.ts
@@ -1,13 +1,37 @@
-import config from '../../config.json';
-import mysql from 'mysql2/promise';
-import { Sequelize } from 'sequelize';
+import config from "../../config.json";
+import mysql from "mysql2/promise";
+import { Sequelize } from "sequelize";
 
 export interface Database {
-    User: any;
+  User: any;
 }
 
-export const db:Database = {} as Database;
+export const db: Database = {} as Database;
 
-export async function initialize() : Promise<void> {
-    const {host, port, user, password, database} = config.database;
+export async function initialize(): Promise<void> {
+  const { host, port, user, password, database } = config.database;
+
+  // Create database if it doesn't exists
+  const connection = await mysql.createConnection({
+    host,
+    port,
+    user,
+    password,
+  });
+  await connection.query(`CREATE DATABASE IF NOT EXISTS \`$(database)\`;`);
+  await connection.end();
+
+  // Connect to databsae with Sequelize
+  const sequelize = new Sequelize(database, user, password, {
+    dialect: "mysql",
+  });
+
+  // Initialize models
+  const { default: userModel } = await import("../users/user.model");
+  db.User = userModel(sequelize);
+
+  // Sync models with database
+  await sequelize.sync({alter: true});
+
+  console.log('Database intitialized and models synced');
 }
